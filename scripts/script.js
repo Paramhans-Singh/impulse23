6a9b97e4b6a6957ec33f0ab7f14eb58511df3707
Karanjot Singh <66475280+karanjot-s@users.noreply.github.com>
2023-03-21 20:41:29 +0530

    added navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,25 @@
 const bg = document.querySelector("main .bg img");
 const events = document.getElementById("events");
 const title = document.querySelector("main .title");
+// const impulse = document.querySelector("main .title h1");
+const headerH1 = document.querySelector("header h1");
 window.onscroll = () => {
   const scroll = document.documentElement.scrollTop;
   bg.style.objectPosition = `${scroll * 0.2}px 74%`;
   events.style.paddingLeft = `${5 - scroll * 0.01}rem`;
   events.style.paddingRight = `${5 - scroll * 0.01}rem`;
   title.style.marginBottom = `${scroll * 0.05}rem`;
+  headerH1.style.top = "-5rem";
+  if (title.getBoundingClientRect().bottom <= 0) {
+    headerH1.style.top = "0";
+  }
+  // impulse.style.position = "static";
+  // impulse.style.fontSize = "6rem";
+  // if (impulse.getBoundingClientRect().top <= 50) {
+  //   impulse.style.position = "fixed";
+  //   impulse.style.top = "0";
+  //   impulse.style.fontSize = "3rem";
+  // }
 };
 
 // const past = document.getElementById("past");
@@ -172,40 +185,6 @@ particleEffect();
 document.addEventListener("DOMContentLoaded", () =>
   requestAnimationFrame(updateTime)
 );
-
-// Set the date we're counting down to
-var countDownDate = new Date("May 12, 2022 09:00:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function () {
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  if (days < 0) days = 0;
-  if (hours < 0) hours = 0;
-  if (minutes < 0) minutes = 0;
-  if (seconds < 0) seconds = 0;
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = `
-  <div class="countdown__count"><div>${days}</div><div class="countdown__text">days</div></div>
-  <div class="countdown__count"><div>${hours}</div><div class="countdown__text">hours</div></div>
-  <div class="countdown__count"><div>${minutes}</div><div class="countdown__text">minutes</div></div>
-  <div class="countdown__count"><div>${seconds}</div><div class="countdown__text">seconds</div></div>`;
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
 
 function updateTime() {
   const currDate = new Date();
