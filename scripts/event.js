286887fe586f829437bf59803aec877ccbe95634
Kushagra <ishaanjain112@gmail.com>
2023-03-25 14:52:12 +0530

    correction in menu toggle

--- scripts/event.js
+++ scripts/event.js
@@ -1,65 +1,67 @@
 ///////////////////////// Menu Toggle /////////////////////////////////
 
-const menuToggle = document.querySelectorAll(".menu-heading");
-const overview = document.getElementById("overview");
-const themes = document.getElementById("themes");
-const schedule = document.getElementById("schedule");
-const prizes = document.getElementById("prizes");
+const menuToggle = document.querySelectorAll('.menu-heading');
+const overview = document.getElementById('overview');
+const themes = document.getElementById('themes');
+const schedule = document.getElementById('schedule');
+const prizes = document.getElementById('prizes');
 
-var list = [overview, themes, schedule, prizes];
+// const section = Array.prototype.slice.call(document.querySelectorAll('.section'));
 
-var qualSelector = document.querySelector(".menu-heading");
+var qualSelector = document.querySelector('.menu-heading');
 var qualContent = overview;
 
-for (var i = 0; i < menuToggle.length; i++) {
-  menuToggle[i].addEventListener("click", function () {
-    qualSelector.classList.remove("selected");
-    this.classList.add("selected");
-    qualSelector = this;
+// for (var i = 0; i < menuToggle.length; i++) {
+// 	menuToggle[i].addEventListener('click', function() {
+// 		qualSelector.classList.remove('selected');
+// 		this.classList.add('selected');
+// 		qualSelector = this;
 
-    qualContent.classList.add("hide");
-    list[i].classList.remove("hide");
-    qualContent = list[i];
-  });
-}
+// 		section.forEach((current) => {
+// 			console.log(current);
+// 			qualContent.classList.add('hide');
+// 			current.classList.remove('hide');
+// 			qualContent = current;
+// 		});
+// 	});
+// }
 
-menuToggle[0].addEventListener("click", function () {
-  qualSelector.classList.remove("selected");
-  this.classList.add("selected");
-  qualSelector = this;
+menuToggle[0].addEventListener('click', function() {
+	qualSelector.classList.remove('selected');
+	this.classList.add('selected');
+	qualSelector = this;
 
-  qualContent.classList.add("hide");
-  overview.classList.remove("hide");
-  qualContent = overview;
+	qualContent.classList.add('hide');
+	overview.classList.remove('hide');
+	qualContent = overview;
 });
 
-menuToggle[1].addEventListener("click", function () {
-  qualSelector.classList.remove("selected");
-  this.classList.add("selected");
-  qualSelector = this;
+menuToggle[1].addEventListener('click', function() {
+	qualSelector.classList.remove('selected');
+	this.classList.add('selected');
+	qualSelector = this;
 
-  qualContent.classList.add("hide");
-  themes.classList.remove("hide");
-  qualContent = themes;
+	qualContent.classList.add('hide');
+	themes.classList.remove('hide');
+	qualContent = themes;
 });
 
-menuToggle[2].addEventListener("click", function () {
-  qualSelector.classList.remove("selected");
-  this.classList.add("selected");
-  qualSelector = this;
+menuToggle[2].addEventListener('click', function() {
+	qualSelector.classList.remove('selected');
+	this.classList.add('selected');
+	qualSelector = this;
 
-  qualContent.classList.add("hide");
-  schedule.classList.remove("hide");
-  qualContent = schedule;
+	qualContent.classList.add('hide');
+	schedule.classList.remove('hide');
+	qualContent = schedule;
 });
 
-menuToggle[3].addEventListener("click", function () {
-  qualSelector.classList.remove("selected");
-  this.classList.add("selected");
-  qualSelector = this;
+menuToggle[3].addEventListener('click', function() {
+	qualSelector.classList.remove('selected');
+	this.classList.add('selected');
+	qualSelector = this;
 
-  qualContent.classList.add("hide");
-  prizes.classList.remove("hide");
-  qualContent = prizes;
+	qualContent.classList.add('hide');
+	prizes.classList.remove('hide');
+	qualContent = prizes;
 });
-ove;
