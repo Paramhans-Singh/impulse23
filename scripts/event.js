f6bed2588e4818d310f050cc7006e0a1cee99231
Karanjot Singh <66475280+karanjot-s@users.noreply.github.com>
2023-03-26 19:36:25 +0530

    added animation and fixed styling in faq

--- scripts/event.js
+++ scripts/event.js
@@ -1,13 +1,13 @@
 ///////////////////////// Menu Toggle /////////////////////////////////
 
-const menuToggle = document.querySelectorAll('.menu-heading');
-const overview = document.getElementById('overview');
-const themes = document.getElementById('themes');
-const schedule = document.getElementById('schedule');
-const prizes = document.getElementById('prizes');
+const menuToggle = document.querySelectorAll(".menu-heading");
+const overview = document.getElementById("overview");
+const themes = document.getElementById("themes");
+const schedule = document.getElementById("schedule");
+const prizes = document.getElementById("prizes");
 
 // const section = Array.prototype.slice.call(document.querySelectorAll('.section'));
-var qualSelector = document.querySelector('.menu-heading');
+var qualSelector = document.querySelector(".menu-heading");
 var qualContent = overview;
 
 // for (var i = 0; i < menuToggle.length; i++) {
@@ -25,59 +25,63 @@ var qualContent = overview;
 // 	});
 // }
 
-menuToggle[0].addEventListener('click', function() {
-	qualSelector.classList.remove('selected');
-	this.classList.add('selected');
-	qualSelector = this;
+menuToggle[0].addEventListener("click", function () {
+  qualSelector.classList.remove("selected");
+  this.classList.add("selected");
+  qualSelector = this;
 
-	qualContent.classList.add('hide');
-	overview.classList.remove('hide');
-	qualContent = overview;
+  qualContent.classList.add("hide");
+  overview.classList.remove("hide");
+  qualContent = overview;
 });
 
-menuToggle[1].addEventListener('click', function() {
-	qualSelector.classList.remove('selected');
-	this.classList.add('selected');
-	qualSelector = this;
+menuToggle[1].addEventListener("click", function () {
+  qualSelector.classList.remove("selected");
+  this.classList.add("selected");
+  qualSelector = this;
 
-	qualContent.classList.add('hide');
-	themes.classList.remove('hide');
-	qualContent = themes;
+  qualContent.classList.add("hide");
+  themes.classList.remove("hide");
+  qualContent = themes;
 });
 
-menuToggle[2].addEventListener('click', function() {
-	qualSelector.classList.remove('selected');
-	this.classList.add('selected');
-	qualSelector = this;
+menuToggle[2].addEventListener("click", function () {
+  qualSelector.classList.remove("selected");
+  this.classList.add("selected");
+  qualSelector = this;
 
-	qualContent.classList.add('hide');
-	schedule.classList.remove('hide');
-	qualContent = schedule;
+  qualContent.classList.add("hide");
+  schedule.classList.remove("hide");
+  qualContent = schedule;
 });
 
-menuToggle[3].addEventListener('click', function() {
-	qualSelector.classList.remove('selected');
-	this.classList.add('selected');
-	qualSelector = this;
+menuToggle[3].addEventListener("click", function () {
+  qualSelector.classList.remove("selected");
+  this.classList.add("selected");
+  qualSelector = this;
 
-	qualContent.classList.add('hide');
-	prizes.classList.remove('hide');
-	qualContent = prizes;
+  qualContent.classList.add("hide");
+  prizes.classList.remove("hide");
+  qualContent = prizes;
 });
 
 ////////////////// FAQ ///////////////
-const quesConts = document.querySelectorAll('.faq-others .ques-cont');
+const quesConts = document.querySelectorAll(".faq-others .ques-cont");
 let open = -1;
 quesConts.forEach((cont, i) => {
-	cont.addEventListener('click', function() {
-		if (open >= 0) {
-			quesConts[open].classList.remove('open');
-		}
-		if (open !== i || open === -1) {
-			cont.classList.add('open');
-			open = i;
-		} else {
-			open = -1;
-		}
-	});
+  const data = cont.querySelector("p");
+  cont.addEventListener("click", function () {
+    if (open >= 0) {
+      const openData = quesConts[open].querySelector("p");
+      quesConts[open].classList.remove("open");
+      openData.style.maxHeight = 0 + "px";
+    }
+    if (open !== i || open === -1) {
+      data.style.maxHeight = data.scrollHeight + "px";
+      cont.classList.add("open");
+      open = i;
+    } else {
+      open = -1;
+    }
+  });
 });
